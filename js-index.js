25b9faf38be55454dfe8da670c06e8525f65aef2
Camilo <riveros2264@gmail.com>
2023-04-24 08:44:00 -0500

    arreglando el mode noche para el nav

--- js-index.js
+++ js-index.js
@@ -1,13 +1,17 @@
 var btn = document.getElementById("btn-toggle");
+var enlaces = document.querySelector("nav");
 
 function toggleBtn(){
     btn.classList.toggle("active");
     
     if(btn.classList.contains("active")){
     document.body.style.backgroundColor ="black";
+    enlaces.style.color ="white";
 
     }else{
         document.body.style.backgroundColor = "white";
+        enlaces.style.color ="black";
+
 
     }
 }
